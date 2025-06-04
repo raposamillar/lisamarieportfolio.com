b5c97088a060bc7398917ee8e4d1dd68f14381a9
raposamillar <lisa.raposamillar@gmail.com>
2025-06-04 19:33:20 -0400

    add hamburger, media queries, and byline JavaScript

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById('byline').innerHTML = "UX Designer Who Codes | Creative Professional | Pianist";
+document.getElementById('byline').innerHTML = `<p>UX Designer Who Codes<br />Creative Professional<br />Pianist</p>`;
+
+function updateByLine() {
+  const byline = document.getElementById('byline');
+  const isSmallScreen = window.matchMedia('(max-width: 555px)').matches;
+
+  if (isSmallScreen) {
+    byline.innerHTML = `<p>UX Designer Who Codes<br />Creative Professional<br />Pianist</p>`;
+  } else {
+    byline.innerHTML = "UX Designer Who Codes | Creative Professional | Pianist";
+  }
+}
+
+updateByLine();
+
+window.addEventListener('resize', updateByLine);
