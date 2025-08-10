468d1b467a5473acdb18f258e4eee9862edf1665
raposamillar <lisa.raposamillar@gmail.com>
2025-08-09 23:47:09 -0400

    move mobile nav and add close btn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 document.getElementById('byline').innerHTML = "UX Designer Who Codes | Creative Professional | Pianist";
 document.getElementById('byline').innerHTML = `<p>UX Designer Who Codes<br />Creative Professional<br />Pianist</p>`;
 
+document.getElementById('close-btn').addEventListener('click', function() {
+  document.getElementById('menu-toggle').checked = false;
+});
+
 function updateByLine() {
   const byline = document.getElementById('byline');
   const isSmallScreen = window.matchMedia('(max-width: 555px)').matches;
